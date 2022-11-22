f777bae3931b0e58b5e4415aa7120df533f3fa38
SergSvr <aveflash@gmail.com>
2022-11-22 17:42:32 +0100

    Lesson 2 fix of double minus double value code optimized

--- src/lesson2/Calculator.java
+++ src/lesson2/Calculator.java
@@ -1,51 +1,55 @@
 package lesson2;
 
+import java.math.BigDecimal;
+
 public class Calculator {
     static double sum(int l1, int l2) {
-        return Double.valueOf(l1) + Double.valueOf(l2);
+        return (double) l1 + l2;
     }
 
     static double sum(long l1, long l2) {
-        return Double.valueOf(l1) + Double.valueOf(l2);
+        return (double) l1 + l2;
     }
 
     static double sum(double l1, double l2) {
-        return Double.valueOf(l1) + Double.valueOf(l2);
+        return l1 + l2;
     }
 
     static double div(int l1, int l2) {
-        return Double.valueOf(l1) / Double.valueOf(l2);
+        return (double) l1 / l2;
     }
 
     static double div(long l1, long l2) {
-        return Double.valueOf(l1) / Double.valueOf(l2);
+        return (double) l1 / l2;
     }
 
     static double div(double l1, double l2) {
-        return Double.valueOf(l1) / Double.valueOf(l2);
+        return l1 / l2;
     }
 
     static double sub(int l1, int l2) {
-        return Double.valueOf(l1) - Double.valueOf(l2);
+        return (double) l1 - l2;
     }
 
     static double sub(long l1, long l2) {
-        return Double.valueOf(l1) - Double.valueOf(l2);
+        return (double) l1 - l2;
     }
 
     static double sub(double l1, double l2) {
-        return Double.valueOf(l1) - Double.valueOf(l2);
+        BigDecimal lb1 = new BigDecimal(String.valueOf(l1));
+        BigDecimal lb2 = new BigDecimal(String.valueOf(l2));
+        return lb1.subtract(lb2).doubleValue();
     }
 
     static double mul(int l1, int l2) {
-        return Double.valueOf(l1) * Double.valueOf(l2);
+        return (double) l1 * l2;
     }
 
     static double mul(long l1, long l2) {
-        return Double.valueOf(l1) * Double.valueOf(l2);
+        return (double) l1 * l2;
     }
 
     static double mul(double l1, double l2) {
-        return Double.valueOf(l1) * Double.valueOf(l2);
+        return l1 * l2;
     }
 }
